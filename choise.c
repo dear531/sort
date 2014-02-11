c71321e3a0d5581d4de7d8869f679f61bdaca232
zly <dear531@21cn.com>
2014-02-11 16:56:30 +0800

    choise of sort

---
+++ choise.c
@@ -0,0 +1,22 @@
+#include "include.h"
+
+void choise(int *a, int n)
+{
+	int i, j, p, t;
+	for(i = 0;i < n - 1; i++)
+	{
+		p = i;
+		for(j = i + 1; j < n; j++)
+		{
+			if(a[p] > a[j])
+				p = j;
+		}
+		if(p != i)
+		{
+			t=a[i];
+			a[i]=a[p];
+			a[p]=t;
+		}
+	}
+	return;
+}
--- include.h
+++ include.h
@@ -1,6 +1,7 @@
 #ifndef __INCLUDE_H__
 #define __INCLUDE_H__
 
-extern void bubble(int *a,int n);
+extern void bubble(int *a, int n);
+extern void choise(int *a, int n);
 
 #endif
