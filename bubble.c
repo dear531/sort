ea2a1f789fedc273394b477db540c415aafb642c
zly <dear531@21cn.com>
2014-02-12 14:54:37 +0800

    add expand bubble sort

--- bubble.c
+++ bubble.c
@@ -1,3 +1,4 @@
+#if 1
 void bubble(int *a, int n)
 {
 	int i, j, t, swp = 1;
@@ -17,3 +18,24 @@ void bubble(int *a, int n)
 	}
 	return;
 }
+#else
+void bubble(int *a, int n)
+{
+	int i, j, t, swp = 1;
+	for(i = 0; i < n - 1 && swp; i++)
+	{
+		swp = 0;
+		for(j = n -1; j > i; j--)
+		{
+			if(a[j - 1] > a[j])
+			{
+				t = a[j - 1];
+				a[j - 1] = a[j];
+				a[j] = t;
+				swp = 1;
+			}
+		}
+	}
+	return;
+}
+#endif
