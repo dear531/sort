7d1cf242e698b8d989efc6f5ebe8d5d2db44e379
zly <dear531@21cn.com>
2014-02-12 10:08:32 +0800

    optimization bubble sort

--- bubble.c
+++ bubble.c
@@ -1,11 +1,9 @@
-#include <stdio.h>
-
 void bubble(int *a, int n)
 {
-	int i, j, t, swp = 0;
-	for(i = 0; i < n - 1 && !swp; i++)
+	int i, j, t, swp = 1;
+	for(i = 0; i < n - 1 && swp; i++)
 	{
-		swp = 1;
+		swp = 0;
 		for(j = 0; j < n - 1 - i; j++)
 		{
 			if(a[j] > a[j+1])
@@ -13,7 +11,7 @@ void bubble(int *a, int n)
 				t = a[j];
 				a[j] = a[j+1];
 				a[j+1] = t;
-				swp = 0;
+				swp = 1;
 			}
 		}
 	}
