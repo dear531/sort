43374bffebf345eca28609d75d40576a1777ac46
zly <dear531@21cn.com>
2014-02-13 17:24:23 +0800

    add expand insert sort

--- insert.c
+++ insert.c
@@ -2,16 +2,12 @@
 void insert(int *a, int n)
 {
 	int i, j, t;
-	for(i = 1;i < n; i++)
+	for (i = 1;i < n; i++)
 	{
 		t = a[i];
-		j = i-1;
-		while(j >= 0 && t < a[j])
-		{
+		for (j = i - 1; j >= 0 && t < a[j]; j--)
 			a[j + 1] = a[j];
-			j--;
-		}
-		a[j+1] = t;
+		a[j + 1] = t;
 	}
 	return;
 }
@@ -21,10 +17,10 @@ void insert(int *a, int n)
 	int i, j, t;
 	for (i = 1;i < n; i++)
 	{
-		t = a[i];
-		for (j = i - 1; j >= 0 && t < a[j]; j--)
-			a[j + 1] = a[j];
-		a[j + 1] = t;
+		t = a[n - 1 - i];
+		for (j = n - i; j < n - 1 && t > a[j]; j++)
+			a[j - 1] = a[j];
+		a[j - 1] = t;
 	}
 	return;
 }
