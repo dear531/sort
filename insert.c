6d1f9cdd28c308dfc94eff32d0d476f8efd10ebf
zly <dear531@21cn.com>
2014-02-14 11:27:18 +0800

    define insert sort form

--- insert.c
+++ insert.c
@@ -1,11 +1,11 @@
-#if 0
+#if 1
 void insert(int *a, int n)
 {
 	int i, j, t;
-	for (i = 1;i < n; i++)
+	for (i = 0;i < n - 1; i++)
 	{
-		t = a[i];
-		for (j = i - 1; j >= 0 && t < a[j]; j--)
+		t = a[i + 1];
+		for (j = i; j >= 0 && t < a[j]; j--)
 			a[j + 1] = a[j];
 		a[j + 1] = t;
 	}
